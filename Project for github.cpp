e00bd0a41f32a02afcb179d1a5bc6f3a0cdcae85
Ahmed farah <ahfr2318@gmail.com>
2025-11-11 18:48:29 +0100

    Refactor clsMathGame and clsScreen classes

--- Project for github.cpp
+++ Project for github.cpp
@@ -1,34 +1,9 @@
 
 #include <iostream>
 #include <iomanip>
+#include "clsScreen"
 
 using namespace std;
-
-//dynamic header sceen to show title a
-class clsScreen
-{
-
-protected :
-
-    void ShowHeaderMenu(string title , string body = "") // I can use this function anytime , because it used parameters ,and I can modify it easier .
-    {
-        time_t T = time(0);
-        tm now;
-        localtime_s(&now,&T);
-
-        short lengthOfTitle = title.length();  // I used 'short' because the numbers won't to be large
-        short lengthOfBody = body.length();
-
-        cout << setw(30) << "" << "-------------------------------------------------------\n";
-        cout << setw(63 - lengthOfTitle) << "" << title << endl;
-        cout << setw(40 - lengthOfBody) << body << endl;
-        cout << setw(30) << "" << "-------------------------------------------------------\n";
-        cout << setw(32) << "" <<
-            (now.tm_mon + 1) << "/" 
-            << (now.tm_mday) << "/" 
-            << (now.tm_year + 1900) << endl;
-    }
-};
 
 //Math game that asks random arithmetic questions with different difficulty levels
 class clsMathGame : protected clsScreen
@@ -256,4 +231,5 @@ int main()
     game.GameStart();
 
     return 0;
+
 }
